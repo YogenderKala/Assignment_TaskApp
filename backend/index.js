b31671e82fff005e5d50b29035e41a3a4c875e67
Yogender Kala <yogenderkala1@gmail.com>
2024-06-23 16:31:10 +0530

    Init

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://yogigotnochill:yogigotnochill@cluster0.ezafbq1.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  dueDate: Date,
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+app.get("/api/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post("/api/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.status(201).json(task);
+});
+
+app.put("/api/tasks/:id", async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(task);
+});
+
+app.delete("/api/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.status(204).send();
+});
+
+app.listen(8000, () => {
+  console.log(`Server is running on port ${8000}`);
+});
